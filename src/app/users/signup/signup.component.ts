b90d29cd32886de616da1de23895cd9acf302542
user.email <tejasv.tagline@gmail.com>
2022-01-13 12:07:57 +0530

    Signup form with bootstrap

--- src/app/users/signup/signup.component.ts
+++ src/app/users/signup/signup.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
+  public roles=['Student','Teacher','User']
   constructor() { }
 
   ngOnInit(): void {
