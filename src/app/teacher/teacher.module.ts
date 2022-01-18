45deaa2d9a83ec6b25f2bc1d7ce94d2e2756ac69
user.email <tejasv.tagline@gmail.com>
2022-01-18 11:05:42 +0530

    created create-exam component on teacher module

--- src/app/teacher/teacher.module.ts
+++ src/app/teacher/teacher.module.ts
@@ -5,12 +5,14 @@ import { TeacherRoutingModule } from './teacher-routing.module';
 import { StudentDataListComponent } from './student-data-list/student-data-list.component';
 import { ViewStudentDetailsComponent } from './view-student-details/view-student-details.component';
 import { UsersModule } from '../users/users.module';
+import { CreateExamComponent } from './create-exam/create-exam.component';
 
 
 @NgModule({
   declarations: [
     StudentDataListComponent,
     ViewStudentDetailsComponent,
+    CreateExamComponent,
   ],
   imports: [
     CommonModule,
