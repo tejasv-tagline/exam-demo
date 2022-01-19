5f1db32a622ede7154f90ba3b026b8b4c76aa718
user.email <tejasv.tagline@gmail.com>
2022-01-19 10:58:46 +0530

    Showed exam details by passing id from teachers side

--- src/app/teacher/view-single-exam-detail/view-single-exam-detail.component.ts
+++ src/app/teacher/view-single-exam-detail/view-single-exam-detail.component.ts
@@ -12,6 +12,8 @@ import { ApiService } from 'src/app/services/api.service';
 export class ViewSingleExamDetailComponent implements OnInit {
   public passId!: string;
   public allQuestionBunch:AllQuestionBunch[]=[];
+  public questionNumbers:number[]=[]
+  // public qn!:any;
 
   constructor(
     private route: ActivatedRoute,
@@ -29,10 +31,19 @@ export class ViewSingleExamDetailComponent implements OnInit {
       next: (res) => {
         if (res.statusCode == 200) {
           this.toaster.success(res.message);
-          console.log('res :>> ', res);
-          console.log('res.data.questions :>> ', res.data.questions);
+          // console.log('res :>> ', res);
+          // console.log('res.data.questions :>> ', res.data.questions);
           this.allQuestionBunch=res.data.questions;
-          console.log('this.allQuestionBunch :>> ', this.allQuestionBunch);
+          // console.log('this.allQuestionBunch :>> ', this.allQuestionBunch);
+          // console.log('res.data.length :>> ', res.data.questions.length);
+          for(let i=0;i<res.data.questions.length;i++){
+            // console.log(typeof i);
+              this.questionNumbers.push(i);
+          }
+          // this.qn=this.questionNumbers.forEach(e=>{console.log('e :>> ', e);})
+          // console.log('this.qn :>> ', this.qn);
+          // console.log('this.questionNumbers :>> ', this.questionNumbers);
+          console.log('res.data.questions:>> ', res.data.questions);
         }
       },
     });
