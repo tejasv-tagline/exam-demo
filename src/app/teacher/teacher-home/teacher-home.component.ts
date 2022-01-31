281fee1baea771424236d5634c870687534bad48
user.email <tejasv.tagline@gmail.com>
2022-01-31 11:29:25 +0530

    Teacher home page modified, added cards on home page for navigation

--- src/app/teacher/teacher-home/teacher-home.component.ts
+++ src/app/teacher/teacher-home/teacher-home.component.ts
@@ -10,9 +10,61 @@ import { LoginComponent } from 'src/app/users/login/login.component';
   styleUrls: ['./teacher-home.component.scss'],
 })
 export class TeacherHomeComponent implements OnInit {
-  public teacherName: string | null= '';
-  public teacherEmail: string | null= '';
+  public teacherName: string | null = '';
+  public teacherEmail: string | null = '';
+  public teacherHome = [
+    {
+      image: '/assets/bg1.jpg',
+      title: 'View All Students Data',
+      text: `To view all student's data click here`,
+      button: `student's data`,
+      routerLink:'studentDataList',
+      class:'btn-outline-light'
+    },
+    {
+      image: '/assets/bg2.jpg',
+      title: 'Create Exam',
+      text: `To create exam click here`,
+      button: `Create exam`,
+      routerLink:'createExam',
+      class:'btn-outline-light'
 
+    },
+    {
+      image: '/assets/bg3.jpg',
+      title: 'View exams',
+      text: `To view all exam's click here`,
+      button: `View exams`,
+      routerLink:'viewExam',
+      class:'btn-outline-light'
+
+    },
+    {
+      image: '/assets/bg4.jpg',
+      title: 'Verified Students',
+      text: `To view all verified student's list click here`,
+      button: `Verified students`,
+      routerLink:'verifyStudentData',
+      class:'btn-outline-light'
+
+    },
+    {
+      image: '/assets/bg5.jpg',
+      title: 'Reset Password',
+      text: `To reset you login password click here`,
+      button: `Reset password`,
+      routerLink:'/resetPassword',
+      class:'btn-outline-warning'
+    },
+    {
+      image: '/assets/bg5.jpg',
+      title: 'Logout',
+      text: `To logout from your account click here`,
+      button: `Logout`,
+      routerLink:'/login',
+      class:'btn-outline-danger'
+    },
+  ];
   constructor() {}
 
   ngOnInit(): void {
